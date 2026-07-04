5e37b4729921ff71dbfcb0f5db901da846e8f9d0
Tanush Mathur <devspark2501@gmail.com>
2026-07-04 12:55:48 +0530

    Redeploy

--- front-end/app/layout.js
+++ front-end/app/layout.js
@@ -1,3 +1,4 @@
+import Footer from "./Components/SectionOne/Footer";
 import NavBar from "./Components/SectionOne/NavBar";
 import "./globals.css";
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
       <body>
         <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
